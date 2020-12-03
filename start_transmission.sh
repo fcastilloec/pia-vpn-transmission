b21c56469fea5b2d455fb06184a08f2202c57129
Felipe Castillo <1280530+fcastilloec@users.noreply.github.com>
2020-12-03 12:29:17 -0800

    feat!: add support for namespaces (#8)
    Both vpn and Transmission are isolated from other processes and network
    connections, which means that the VPN can stayed connected all the time
    without interfering with local servers. Transmission web interface is
    still accessible to the local network.
    
    BREAKING CHANGE: PIA vpn and Transmission now run under a namespace
    
    Any new process that requires a connection to the VPN will need to be
    run inside the namespace

--- start_transmission.sh
+++ start_transmission.sh
@@ -12,9 +12,19 @@ failure() {
 trap 'failure ${LINENO} "$BASH_COMMAND"' ERR
 
 ############### CHECKS & VARIABLES ###############
+# Check if running as root/sudo
+[ ${EUID:-$(id -u)} -eq 0 ] || exec sudo -E "$(readlink -f "$0")" "$@"
+
 # Checks if jq is installed
 if ! command -v jq &>/dev/null; then
   echo "jq could not be found"; echo "Please install jq"; exit 1
+fi
+
+# Check if the mandatory environment variables are set.
+if [[ -z $NETNS_NAME ]]; then
+  echo "$(basename "$0") script requires:"
+  echo "NETNS_NAME  - name of the namespace"
+  exit 1
 fi
 
 SETTINGS=/home/felipe/.config/transmission/settings.json
@@ -26,14 +36,11 @@ if [[ ! -f $SETTINGS ]]; then
   >&2 echo "Cannot modify Transmission settings. $SETTINGS not found"; exit 1
 fi
 
-# Check if running as root/sudo
-[ ${EUID:-$(id -u)} -eq 0 ] || exec sudo -E "$(readlink -f "$0")" "$@"
-
 ##########################################
 # Stop transmission-daemon if it's active
-if systemctl -q is-active transmission-daemon.service; then
+if pidof transmission-daemon > /dev/null; then
   if [[ -n $DEBUG ]]; then echo "transmission-daemon is active. Stopping..."; fi
-  set +e; systemctl stop transmission-daemon.service; set -e
+  kill -9 "$(pidof transmission-daemon)"
 fi
 
 # Modifies the Transmission settings with the new port
@@ -41,8 +48,11 @@ tempSettings=$(jq '."peer-port"'="$PORT" $SETTINGS)
 printf "%s" "$tempSettings" > $SETTINGS
 
 # Adds the port to UFW
-ufw allow "$PORT/tcp" > /dev/null
+# ufw allow "$PORT/tcp" > /dev/null
 echo "VPN port forwarded, port $PORT is being used"
 
 # Re-start transmission
-systemctl start transmission-daemon.service
+ip netns exec "$NETNS_NAME" sudo -u felipe /usr/bin/transmission-daemon -g /home/felipe/.config/transmission
+
+# Start redirection to access web interface
+socat tcp-listen:9091,reuseaddr,fork,range=192.168.1.0/26 tcp-connect:192.168.100.2:9091 > /dev/null 2>&1 &
