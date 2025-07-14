48d2dca06b3533ec66dbcd21f0ac06db07b2f59c
Felipe Castillo <fcastillo.ec@gmail.com>
2025-07-14 15:58:33 -0400

    fix: better checks if docker compose project is not running

--- set_port.sh
+++ set_port.sh
@@ -1,24 +1,32 @@
 #!/usr/bin/env bash
 
+set -e
 trap 'exit 0' SIGTERM
 
-root_dir=$(/usr/local/bin/docker-compose ls | grep pia-transmission-prowlarr | awk '{print $3}' | xargs dirname)
-readonly root_dir
-readonly transmission_port="${root_dir}/data/pia-port/port.dat"
+readonly NAME="media-management"
+config_file=$(
+  /usr/local/bin/docker-compose ls --format json --filter name="${NAME}" \
+    | jq -r 'select(length > 0) | .[0].ConfigFiles // empty'
+)
 
 ## Checks
-if [[ -z ${root_dir} ]]; then
-  >&2 echo "Docker compose project is not running"; exit 1
+if [[ -z ${config_file} ]]; then
+  >&2 echo "Docker compose project is not running"
+  exit 0
 fi
+transmission_port="$(dirname "${config_file}")/data/pia-port/port.dat"
+readonly transmission_port
 
 if ! [[ -f ${transmission_port} && -r ${transmission_port} ]]; then
-  >&2 echo "Port file doesn't exist or is not readable!"; exit 1
+  >&2 echo "Port file doesn't exist or is not readable!"
+  exit 1
 fi
 
 sleep 5 # give some time for Transmission to start
 
-if ! transmission-remote -l >/dev/null 2>&1; then # Checks that Transmission is running
-  echo "Transmission not running."; exit 1
+if ! transmission-remote -l > /dev/null 2>&1; then # Checks that Transmission is running
+  echo "Transmission not running."
+  exit 1
 fi
 
 # Reading port
@@ -30,7 +38,8 @@ transmission-remote -p "${PORT}"
 sleep 2
 # Testing port
 if [[ $(transmission-remote -pt) != "Port is open: Yes" ]]; then
-  echo "Transmission port (${PORT}) is not open!"; exit 1
+  echo "Transmission port (${PORT}) is not open!"
+  exit 1
 fi
 
 exit 0
